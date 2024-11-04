572da712e1deb5c40fc6f1b478d4621e8b5b7298
wacho91 <criskol.71@gmail.com>
2024-11-04 11:39:06 -0500

    Configuration darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Navbar from "./Component/Navbar/Navbar";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function App() {
+
+  // dark mode start
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  const element = document.documentElement;
+
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  },[theme]);
+  // dark mode end
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  },[]);
+
   return (
-    <div className="">
-        <Navbar />
+    <div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
+        <Navbar theme={theme} setTheme={setTheme} />
     </div>
   )
 }
