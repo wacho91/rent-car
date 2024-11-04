8ddea4f75095dc411a058939b70bdb26e14cab75
wacho91 <criskol.71@gmail.com>
2024-11-04 11:30:44 -0500

    Arquitecture Navbar Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./Component/Navbar/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="">
+        <Navbar />
+    </div>
   )
 }
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+
+export const Navlinks = [
+    {
+      id: 1,
+      name: "HOME",
+      link: "/#",
+    },
+    {
+      id: 2,
+      name: "CARS",
+      link: "/#cars",
+    },
+    {
+      id: 1,
+      name: "ABOUT",
+      link: "/#about",
+    },
+    {
+      id: 1,
+      name: "BOOKING",
+      link: "/#booking",
+    },
+];
+
+const Navbar = () => {
+  return (
+    <div className="relative z-10 shadow-md w-full dark:bg-black dark:text-white duration-300">
+        <div className="container py-2 md:py-0">
+            <div className="flex justify-between items-center">
+                <div>
+                    <span className="text-3xl font-bold font-serif">Car Rental</span>
+                </div>
+                <nav className="hidden md:block">
+                    <ul className="flex items-center gap-8">
+                        {
+                            Navlinks.map(({id, name, link}) => (
+                                <li key={id} className="py-4">
+                                    <a 
+                                        href={link}
+                                        className="text-lg font-medium hover:text-primary py-2 hover:border-b-2 hover:border-primary transition-colors duration-500"
+                                    >
+                                        {name}
+                                    </a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
