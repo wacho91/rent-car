62eb75fbf25d0df64eb142304dbc469ccd69cd7e
wacho91 <criskol.71@gmail.com>
2024-11-05 11:25:51 -0500

    Styles in the services component

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -36,20 +36,23 @@ const Services = () => {
   return (
     <>
         <span id="about"></span>
-        <div>
-            <div>
-                <div>
-                    <h1>
+        <div className="dark:bg-black dark:text-white py-14 sm:min-h-[600px] sm:grid sm:place-items-center">
+            <div className="container">
+                <div className="pb-12">
+                    <h1 className="text-3xl font-semibold text-center sm:text-4xl font-serif">
                         Why Choose Us
                     </h1>
                 </div>
-                <div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                     {skillsData.map((skill) => (
-                        <div key={skill.name}>
-                            <div>{skill.icon}</div>
-                            <h1>{skill.name}</h1>
+                        <div 
+                            key={skill.name}
+                            className="card text-center group space-y-3 sm:space-y-6 p-4 sm:py-16 bg-dark  hover:bg-primary duration-300 text-white hover:text-black rounded-lg"
+                        >
+                            <div className="grid place-items-center">{skill.icon}</div>
+                            <h1 className="text-2xl font-bold">{skill.name}</h1>
                             <p>{skill.description}</p>
-                            <a href={skill.link}>
+                            <a href={skill.link} className="inline-block text-lg font-semibold py-3 text-primary group-hover:text-black duration-300">
                                 Learn More 
                             </a>
                         </div>
