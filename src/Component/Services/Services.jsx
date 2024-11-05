582ccec95c7e89a18c871051998aae774417e8b5
wacho91 <criskol.71@gmail.com>
2024-11-05 11:21:03 -0500

    Arquitecture Services Complete

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -1,10 +1,63 @@
+import { FaCameraRetro } from "react-icons/fa"
+import { GiNotebook } from "react-icons/gi"
+import { SlNote } from "react-icons/sl"
 
+const skillsData = [
+    {
+      name: "Best Price",
+      icon: (
+        <FaCameraRetro className="text-5xl text-primary group-hover:text-black duration-300" />
+      ),
+      link: "#",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      aosDelay: "0",
+    },
+    {
+      name: "Fast and Safe",
+      icon: (
+        <GiNotebook className="text-5xl text-primary group-hover:text-black duration-300" />
+      ),
+      link: "#",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      aosDelay: "500",
+    },
+    {
+      name: "Experience Drivers",
+      icon: (
+        <SlNote className="text-5xl text-primary group-hover:text-black duration-500" />
+      ),
+      link: "#",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      aosDelay: "1000",
+    },
+];
 
 const Services = () => {
   return (
-    <div>
-        Services Component
-    </div>
+    <>
+        <span id="about"></span>
+        <div>
+            <div>
+                <div>
+                    <h1>
+                        Why Choose Us
+                    </h1>
+                </div>
+                <div>
+                    {skillsData.map((skill) => (
+                        <div key={skill.name}>
+                            <div>{skill.icon}</div>
+                            <h1>{skill.name}</h1>
+                            <p>{skill.description}</p>
+                            <a href={skill.link}>
+                                Learn More 
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
