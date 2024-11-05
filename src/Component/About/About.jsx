4eab8ae69b2ca710bfdeabdfb7c1ce820f648a84
wacho91 <criskol.71@gmail.com>
2024-11-05 11:10:20 -0500

    Animation About Complete

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="dark:bg-dark bg-slate-100 sm:min-h-[600px] sm:grid sm:place-items-center duration-300">
         <div className="container">
             <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
-                <div>
+                <div data-aos="slide-right" data-aos-duration="1500">
                     <img 
                         src={carPng} 
                         alt="imageCar"
@@ -14,18 +14,21 @@ const About = () => {
                     />
                 </div>
                 <div className="space-y-5 sm:p-16 pb-6">
-                    <h1 className="text-3xl sm:text-4xl font-bold font-serif">
+                    <h1 
+                        data-aos="fade-up"
+                        className="text-3xl sm:text-4xl font-bold font-serif"
+                    >
                         About us
                     </h1>
-                    <p className="leading-8 tracking-wide">
+                    <p data-aos="fade-up" className="leading-8 tracking-wide">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                         Aspernatur, magnam! Tenetur odio quo et maxime?
                     </p>
-                    <p>
+                    <p data-aos="fade-up">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
                         tempora.
                     </p>
-                    <button className="button-outline">
+                    <button data-aos="fade-up" className="button-outline">
                         Get Started
                     </button>
                 </div>
