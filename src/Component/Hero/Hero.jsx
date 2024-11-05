d732f25b1786ff62bb3496dc970a26a32fe98766
wacho91 <criskol.71@gmail.com>
2024-11-05 10:53:04 -0500

    Arwuitecture Hero complete

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,12 +1,18 @@
 
 import carPng from "../../assets/car.png";
 import yellowCar from "../../assets/banner-car.png";
+import AOS from "aos";
+import { useEffect } from "react";
 
 const Hero = ({theme}) => {
+
+    useEffect(() => {
+        AOS.refresh();
+    })
   return (
     <div className="dark:bg-black dark:text-white duration-300">
         <div className="container min-h-[620px] flex">
-            <divc className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
                 <div className="order-1 sm:order-2">
                     <img 
                         src={theme === "dark" ? yellowCar : carPng} 
@@ -15,11 +21,21 @@ const Hero = ({theme}) => {
                     />
                 </div>
                 <div>
-                    <p>
+                    <p className="text-primary text-2xl font-bold">
                         Effortless
                     </p>
+                    <h1 className="text-5xl lg:text-7xl font-semibold font-serif">
+                        Car Rental
+                    </h1>
+                    <p className="">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+                        veritatis explicabo quibusdam quae reprehenderit ab{" "}
+                    </p>
+                    <button className="rounded-md bg-primary hover:bg-primary/80 transition duration-500 py-2 px-6 text-black">
+                        Get Started
+                    </button>
                 </div>
-            </divc>
+            </div>
         </div>
     </div>
   )
