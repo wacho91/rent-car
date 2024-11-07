b49714d7ee624a42d1e82d9313d46fb20d34c0ba
wacho91 <criskol.71@gmail.com>
2024-11-07 11:01:31 -0500

    Animation Component

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <>
         <span id="contact"></span>
-        <div className="dark:bg-black dark:text-white py-14">
+        <div data-aos="zoom-in" className="dark:bg-black dark:text-white py-14">
             <div className="container">
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
                     <div className="col-span-2 space-y-3">
