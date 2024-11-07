d7243863d9e0a71c621a73220e9ec361cf5d9aa7
wacho91 <criskol.71@gmail.com>
2024-11-07 10:59:49 -0500

    Contact Component complete

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -3,21 +3,24 @@
 const Contact = () => {
   return (
     <>
-        <span id="about"></span>
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <h1>
+        <span id="contact"></span>
+        <div className="dark:bg-black dark:text-white py-14">
+            <div className="container">
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
+                    <div className="col-span-2 space-y-3">
+                        <h1 className="text-4xl sm:text-5xl font-bold text-white">
                             Let's collaborate on your upcoming car rental venture
                         </h1>
-                        <p>
+                        <p className="text-gray-400">
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                             Exercitationem necessitatibus quasi et vel,{" "}
                         </p>
                     </div>
-                    <div>
-                        <a href="">
+                    <div className="sm:grid sm:place-items-center">
+                        <a 
+                            href="#"
+                            className="inline-block font-semibold py-2 px-6 bg-primary text-white hover:bg-primary/80 duration-200 tracking-widest uppercase "    
+                        >
                             Contact
                         </a>
                     </div>
